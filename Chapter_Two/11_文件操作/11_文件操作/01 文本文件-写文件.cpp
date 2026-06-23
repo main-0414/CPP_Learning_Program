324d14dc202448befe8b9f4d7c50043925232272
观观王 <zhuaige183@163.com>
2026-06-23 19:05:19 +0800

    6.23

---
+++ Chapter_Two/11_文件操作/11_文件操作/01 文本文件-写文件.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <fstream>	// 头文件的包含
+using namespace std;
+
+
+void test01()
+{
+	// 1.包含头文件的对象
+
+
+	// 2.创建流对象
+
+	ofstream ofs;
+
+	// 3.指定打开方式
+	ofs.open("test.txt", ios::out);		// 程序往文件里输出数据，就是写入
+
+	// 4.写内容
+	ofs << "姓名：张三" << endl;
+	ofs << "性别：男" << endl;
+	ofs << "年龄：18" << endl;
+
+	// 5.关闭文件
+	ofs.close();
+
+}
+
+
+
+
+
+
+
+
+int main()
+{
+	test01();
+
+
+	system("pause");
+}
+
