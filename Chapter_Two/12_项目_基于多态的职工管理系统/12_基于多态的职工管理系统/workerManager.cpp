827f54b338d479ab6aa23ad884bda876a0202a82
观观王 <zhuaige183@163.com>
2026-06-26 23:11:28 +0800

    6.26 新建职工功能

--- Chapter_Two/12_项目_基于多态的职工管理系统/12_基于多态的职工管理系统/workerManager.cpp
+++ Chapter_Two/12_项目_基于多态的职工管理系统/12_基于多态的职工管理系统/workerManager.cpp
@@ -2,7 +2,9 @@
 
 workerManager::workerManager()
 {
-
+	// 初始化属性
+	this->m_EmpNum = 0;
+	this->m_EmpArray = nullptr;
 }
 
 void workerManager::showMenu()
@@ -22,12 +24,119 @@ void workerManager::showMenu()
 }
 
 
+// 添加职工
+void workerManager::Add_Emp()
+{
+	cout << "请输入添加职工的数量：" << endl;
+
+	int addNum = 0;		// 保存用户输入增加的数量
+	cin >> addNum;
+
+	if (addNum > 0)
+	{
+		// 添加
+		// 计算新建空间大小
+		int newSize = this->m_EmpNum + addNum;	// 新建空间大小 = 原记录人数 + 新增人数
+
+		// 开辟新空间
+		Worker** newSpace = new Worker* [newSize];
+
+		// 将原空间下的数据，拷贝到新空间下
+		if (this->m_EmpArray != nullptr)
+		{
+			for (int i = 0; i < this->m_EmpNum; i++)
+			{
+				newSpace[i] = this->m_EmpArray[i];
+			}
+		}
+
+		// 批量添加新数据
+		for (int i = 0; i < addNum; i++)
+		{
+			int ID;			// 职工编号
+			string name;	// 职工姓名
+			int deptSelect;	// 部门选择
+
+			cout << "请输入第" << i + 1 << "个新职工编号：" << endl;
+			cin >> ID;
+
+			cout << "请输入第" << i + 1 << "个新职工姓名：" << endl;
+			cin >> name;
+
+			cout << "请输入第" << i + 1 << "个新职工部门：" << endl
+				<< "1、普通员工" << endl
+				<< "2、经理" << endl
+				<< "3、总裁" << endl;
+
+			cin >> deptSelect;
+
+
+			// 不同部门选择要new出不同的对象
+			Worker* nWorker = nullptr;
+
+
+
+			switch (deptSelect)
+			{
+			case 1:
+				nWorker = new Employee(ID, name, 1);
+				break;
+
+			case 2:
+				nWorker = new Manager(ID, name, 2);
+				break;
+
+			case 3:
+				nWorker = new Boss(ID, name, 3);
+				break;
+
+			default:
+				break;
+			}
+
+
+			// 将创建职工职责，保存到数组中
+			newSpace[this->m_EmpNum + i] = nWorker;
+
+
+
+			// 释放原有空间
+			delete[] this->m_EmpArray;
+
+			// 更新新空间的指向
+			this->m_EmpArray = newSpace;
+
+
+			// 更新新人数空间大小
+			this->m_EmpNum = newSize;
+		}
+
+
+
+	}
+	else
+	{
+		cout << "输入数据有误" << endl;
+	}
+}
+
+
+
+
+
+
+
 void workerManager::exitSystem()
 {
 	cout << "欢迎下次使用" << endl;
 	system("pause");
 	exit(0);	// 退出程序
 }
+
+
+
+
+
 
 
 
--- Chapter_Two/12_项目_基于多态的职工管理系统/12_基于多态的职工管理系统/workerManager.h
+++ Chapter_Two/12_项目_基于多态的职工管理系统/12_基于多态的职工管理系统/workerManager.h
@@ -1,6 +1,10 @@
 ﻿#pragma once			// 防止头文件重复包含
-#include <iostream>		// 包含输入输出流头文件
-using namespace std;	// 使用标准的命名空间
+#include <iostream>
+#include "worker.h"
+#include "employee.h"
+#include "manager.h"
+#include "boss.h"
+using namespace std;
 
 
 class workerManager
@@ -15,7 +19,14 @@ public:
 	// 退出功能
 	void exitSystem();
 
+	// 记录职工人数
+	int m_EmpNum;
 
+	// 职工数组指针
+	Worker** m_EmpArray;
+
+	// 添加员工
+	void Add_Emp();
 	
 	// 析构函数
 	~workerManager();
