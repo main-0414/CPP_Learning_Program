a487e48174182d673ba1e454969e68f3701856e5
观观王 <zhuaige183@163.com>
2026-07-10 19:36:35 +0800

    7.10 template

---
+++ Chapter_Three/01_函数模板语法/01_函数模板语法/01_函数模板语法/02 函数模板注意事项.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+using namespace std;
+
+
+// 函数模板注意事项
+
+template<typename T>// typename 可以替换为 class
+void mySwap(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+
+// 1.自动类型推导，必须推导出一致的数据类型 T 才能使用
+void test02()
+{
+	int a = 10;
+	int b = 20;
+	char c = 'c';
+
+	//mySwap(a, b);	// 正确
+	//mySwap(a, c);
+
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+}
+
+
+
+// 2.模板必须要确定出T的数据类型，才可以使用
+template<class T>
+void func()	// 紧跟模板定义的函数就是函数模板了
+{
+	cout << "func() 调用" << endl;
+
+}
+
+void test03()
+{
+	//func();	// 报错了，此时编译器没办法猜出数据类型，只能手动告诉
+	func<int>();
+}
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+	test02();
+	test03();
+
+
+	system("pause");
+	return 0;
+}
---
+++ Chapter_Three/01_案例_不同数据类型数组排序/01_案例_不同数据类型数组排序/main.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+案例描述：
+	利用函数模板封装一个排序的函数，可以对不同数据类型数组进行排序
+	排序规则 从大到小，排序算法为 选择排序
+	分别用 char数组 和 int数组 进行测试
+*/
+
+
+// 排序算法
+template<class T>
+void mySort(T arr[], int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		int MAX = i;
+		for (int j = i + 1; j < len; j++)
+		{
+			if (arr[MAX] < arr[j])
+			{
+				MAX = j;
+			}
+		}
+
+		if (MAX != i)
+		{
+			int temp = arr[i];
+			arr[i] = arr[MAX];
+			arr[MAX] = temp;
+		}
+
+	}
+}
+
+// 提供打印数组模板
+template<class T>
+void printArr(T arr[], int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		cout << arr[i] << " " ;
+	}
+	cout << endl;
+}
+
+
+
+
+// 测试char数组
+void test01()
+{
+	char charArr[] = "badcfe";
+	int num = sizeof(charArr) / sizeof(char);
+	mySort(charArr, num);
+	printArr(charArr, num);
+}
+
+// 测试int数组
+void test02()
+{
+	int intArr[] = {5,9,6,3,2,7,1,8};
+	int num = sizeof(intArr) / sizeof(int);
+	mySort(intArr, num);
+	printArr(intArr, num);
+}
+
+
+
+
+
+
+
+int main()
+{
+	test01();
+	test02();
+
+
+
+	system("pause");
+	return 0;
+}
