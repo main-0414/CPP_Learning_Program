8a4e2663e0744526f4712e2fca2d87defb9c51ea
观观王 <zhuaige183@163.com>
2026-07-16 00:48:34 +0800

    7.15 类模板

---
+++ Chapter_Three/02_类模板语法/02_类模板语法/01 类模板语法.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+// 类模板
+template<class NameType, class AgeType>
+
+class Person
+{
+public:
+	Person(NameType name, AgeType age)
+	{
+		m_Name = name;
+		m_age = age;
+	}
+
+	void showPerson()
+	{
+		cout << "name：" << this->m_Name << " age：" << this->m_age << endl;
+	}
+
+
+
+	NameType m_Name;
+	AgeType m_age;
+};
+
+
+
+
+
+void test01()
+{
+	Person<string, int> p1("孙悟空", 999);
+
+	p1.showPerson();
+
+}
+
+
+// 总结：类模板和函数模板语法极其相似，在声明模板template后面加类，此类成为类模板
+
+
+
+int main1()
+{
+	test01();
+
+
+	system("pause");
+	return 0;
+}
---
+++ Chapter_Three/02_类模板语法/02_类模板语法/02 类模板 与 函数模板 的区别.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+// 类模板 和 函数模板的区别
+
+template<class NameType,class AgeType>
+class Person
+{
+public:
+	Person(NameType name, AgeType age)
+	{
+		m_Name = name;
+		m_Age = age;
+	}
+
+	void showPerson()
+	{
+		cout << "name：" << this->m_Name << " age：" << this->m_age << endl;
+	}
+	
+
+
+	NameType m_Name;
+	AgeType m_Age;
+};
+
+
+
+
+// 1. 类模板没有自动类型推导使用方式
+// 2. 类模板在模板参数列表
+
+
+void test02()
+{
+	
+
+
+
+
+
+
+}
+
+
+
+
+
+int main()
+{
+
+
+
+
+	system("pause");
+	return 0;
+}
