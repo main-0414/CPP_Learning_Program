ac4e35ea25110330405aafc22633cf564fdaee62
观观王 <zhuaige183@163.com>
2026-07-16 15:22:01 +0800

    7.16 类模板

--- Chapter_Three/02_类模板语法/02_类模板语法/02 类模板 与 函数模板 的区别.cpp
+++ Chapter_Three/02_类模板语法/02_类模板语法/02 类模板 与 函数模板 的区别.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 // 类模板 和 函数模板的区别
 
-template<class NameType,class AgeType>
+template<class NameType, class AgeType = int>		// 尖括号里面的是模板参数列表
 class Person
 {
 public:
@@ -17,9 +17,9 @@ public:
 
 	void showPerson()
 	{
-		cout << "name：" << this->m_Name << " age：" << this->m_age << endl;
+		cout << "name：" << this->m_Name << " age：" << this->m_Age << endl;
 	}
-	
+
 
 
 	NameType m_Name;
@@ -29,13 +29,27 @@ public:
 
 
 
-// 1. 类模板没有自动类型推导使用方式
-// 2. 类模板在模板参数列表
-
-
+// 1. 类模板没有自动类型推导使用方式(新版本可以)
 void test02()
 {
-	
+	Person p1("孙悟空", 1000);
+	p1.showPerson();		// 实则新版本可以
+
+	Person<string, int> p2("观观王", 10000);
+	p2.showPerson();
+
+
+}
+
+
+// 2. 类模板在模板参数列表
+void test03()
+{
+	Person<string> p("猪八戒", 9999);		// 我只指定了第一个，第二个是使用了默认参数的类型
+	p.showPerson();
+
+
+
 
 
 
@@ -46,12 +60,10 @@ void test02()
 
 
 
-
-
 int main()
 {
-
-
+	test02();
+	test03();
 
 
 	system("pause");
