c7baee1c1746c51fc2da4c68ac7cfb29680dc637
观观王 <zhuaige183@163.com>
2026-07-23 19:26:06 +0800

    7.23 案例

--- Chapter_Three/02_案例_类模板增删改查/02_案例_类模板增删改查/main.cpp
+++ Chapter_Three/02_案例_类模板增删改查/02_案例_类模板增删改查/main.cpp
@@ -17,11 +17,12 @@ using namespace std;
 void test01()
 {
 	MyArray <int>arr1(5);
-
-	//MyArray<int>arr2(arr1);
-
-	//MyArray<int>arr3(100);
 	arr1 = arr1;
+
+	MyArray<int>arr2(arr1);
+
+	MyArray<int>arr3(100);
+	arr3 = arr1;
 }
 
 
