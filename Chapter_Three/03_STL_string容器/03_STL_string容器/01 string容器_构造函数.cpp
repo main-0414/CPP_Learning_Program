73e3af40532bbc8fbf909a8f92fc7293669f9cdd
观观王 <zhuaige183@163.com>
2026-07-29 18:17:46 +0800

    7.29 string容器赋值操作

--- Chapter_Three/03_STL_string容器/03_STL_string容器/01 string容器_构造函数.cpp
+++ Chapter_Three/03_STL_string容器/03_STL_string容器/01 string容器_构造函数.cpp
@@ -19,16 +19,16 @@ void test01()
 
 	cout << "str2 = " << s2 << endl;
 
+	string s3(s2);
+	cout << "str3 = " << s3 << endl;
+
+	string s4(10, 'a');
+	cout << "str4 = " << s4 << endl;
 }
 
 
 
-
-
-
-
-
-int main()
+int main1()
 {
 	test01();
 
