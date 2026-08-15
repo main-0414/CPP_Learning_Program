a4ec5a5435b5f72a21170e60403e085a8e985289
观观王 <zhuaige183@163.com>
2026-08-15 16:01:57 +0800

    8.15 queue队列

--- Chapter_Three/03_STL_queue容器/03_STL_queue容器/01 基本概念 与 接口.cpp
+++ Chapter_Three/03_STL_queue容器/03_STL_queue容器/01 基本概念 与 接口.cpp
@@ -25,11 +25,13 @@ void test01()
 {
 	queue<Person> q;
 
+
 	// 准备数据
 	Person p1("唐僧", 30);
 	Person p2("孙悟空", 1000);
 	Person p3("猪八戒", 500);
 	Person p4("沙僧", 200);
+
 
 	// 入队
 	q.push(p1);		// 唐僧先入队，先出
@@ -38,17 +40,22 @@ void test01()
 	q.push(p4);
 
 
+
 	// 先查看队列大小
 	cout << "队列大小：" << q.size() << endl;
+
 
 
 	// 判断只要队列不为空，查看队头，查看队尾，出队
 	while (!q.empty())
 	{
 		// 查看队头
-		cout << "队头元素 ———— 姓名：" << q.front().m_Name << " 队头元素 ———— 年龄：" << q.front().m_Age << endl;
+		cout << "队头元素 ———— 姓名：" << q.front().m_Name 
+			<< " 队头元素 ———— 年龄：" << q.front().m_Age << endl;
+
 		// 查看队尾
-		cout << "队尾元素 ———— 姓名：" << q.back().m_Name << " 队尾元素 ———— 年龄：" << q.back().m_Age << endl;
+		cout << "队尾元素 ———— 姓名：" << q.back().m_Name 
+			<< " 队尾元素 ———— 年龄：" << q.back().m_Age << endl;
 
 		// 出队
 		q.pop();
