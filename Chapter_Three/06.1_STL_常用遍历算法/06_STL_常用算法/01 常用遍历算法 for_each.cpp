26349f32dd32113d27167f96a56c02c922e2fa65
观观王 <zhuaige183@163.com>
2026-08-23 15:48:51 +0800

    8.23 常用遍历算法

---
+++ Chapter_Three/06.1_STL_常用遍历算法/06_STL_常用算法/01 常用遍历算法 for_each.cpp
@@ -0,0 +1,74 @@
+﻿// 概述:
+// 算法主要是由头文件 <algorithm> <functional> <numeric> 组成。
+
+// <algorithm > 是所有STL头文件中最大的一个，范围涉及到比较、 交换、查找、遍历操作、复制、修改等等
+// <numeric > 体积很小，只包括几个在序列上面进行简单数学运算的模板函数
+// <functional > 定义了一些模板类, 用以声明函数对象。
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+// for_each(iterator beg, iterator end, _func);
+
+// 遍历算法 遍历容器元素
+// beg 开始迭代器
+// end 结束迭代器
+// _func 函数或者函数对象
+
+
+
+// 普通函数
+void print01(int val)
+{
+	cout << val << " ";
+}
+
+
+// 仿函数
+class print02
+{
+public:
+	void operator()(int val)
+	{
+		cout << val << " ";
+	}
+};
+
+
+
+//  常用遍历算法  for_each
+void test01()
+{
+	vector<int> v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+	}
+	
+	for_each(v.begin(), v.end(), print01);
+	// for_each(起始迭代器，结束迭代器，函数or仿函数)
+
+	cout << endl;
+
+	for_each(v.begin(), v.end(), print02());
+
+	cout << endl;
+}
+
+
+// 总结：for_each很常用！！
+
+
+
+
+
+int main1()
+{
+	test01();
+
+	system("pause");
+	return 0;
+}
---
+++ Chapter_Three/06.1_STL_常用遍历算法/06_STL_常用算法/02 常用遍历算法 transform.cpp
@@ -0,0 +1,82 @@
+﻿// transform(iterator beg1, iterator end1, iterator beg2, _func);
+
+//beg1 源容器开始迭代器
+//end1 源容器结束迭代器
+//beg2 目标容器开始迭代器
+//_func 函数或者函数对象
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#include <vector>
+
+// 仿函数
+class Transform
+{
+public:
+	int operator()(int val)	// 先取到，逻辑运算后返回
+	{
+		return val + 100;	// 灵活，如果想要原封不动搬运就直接返回，如果在过程中想有一些不同，允许操作
+	}
+};
+
+// 普通函数
+int TransformFunc(int val)
+{
+	return val + 100;
+}
+
+
+
+class print
+{
+public:
+	void operator()(int val)
+	{
+		cout << val << " ";
+	}
+};
+
+
+
+void test02()
+{
+	vector<int> v;	// 原容器
+
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+	}
+
+	vector<int> vTarget;	// 目标容器
+	vTarget.resize(10);		// 目标容器需要提前开辟空间
+
+	//transform(v.begin(), v.end(), vTarget.begin(), Transform());
+	//transform(v.begin(), v.end(), vTarget.begin(), TransformFunc);
+	transform(v.begin(), 
+		v.end(), 
+		vTarget.begin(), 
+		[](int val)
+		{
+			return val + 100;
+		}
+		);
+
+	for_each(vTarget.begin(), vTarget.end(),print());
+
+	cout << endl;
+
+}
+
+
+
+
+
+int main()
+{
+	test02();
+
+	system("pause");
+	return 0;
+}
