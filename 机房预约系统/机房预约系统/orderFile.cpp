7d74b340953ed678e2cb90b7a28db980a0fadfff
观观王 <zhuaige183@163.com>
2026-09-03 19:53:58 +0800

    9.3 一些student功能

---
+++ 机房预约系统/机房预约系统/orderFile.cpp
@@ -0,0 +1,14 @@
+﻿#include "orderFile.h"
+
+// 构造函数
+OrderFile::OrderFile()
+{
+
+}
+
+// 更新预约记录
+void OrderFile::updataOrder()
+{
+
+}
+
---
+++ 机房预约系统/机房预约系统/orderFile.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+using namespace std;
+#include <iostream>
+#include <map>
+#include "globalFile.h"
+
+class OrderFile
+{
+public:
+
+	// 构造函数
+	OrderFile();
+
+	// 更新预约记录
+	void updataOrder();
+
+	// 申请记录的容器		key ----- 哪一条？		value ----- 条内信息
+	map<int, map<string, string>> m_orderData;
+
+	// 预约记录条数
+	int m_Size();
+
+
+};
